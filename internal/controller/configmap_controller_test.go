a14773e5358ad3cfd766c51efce251e5b8274d7f
Felicia Lyn-Shue <flynshue@gmail.com>
2024-09-13 15:19:04 -0400

    really rough draft of first test case

--- internal/controller/configmap_controller_test.go
+++ internal/controller/configmap_controller_test.go
@@ -19,6 +19,7 @@ package controller
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -26,52 +27,74 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-)
-
-const (
-	testSrcNamespace = "test-src"
-	testSrcConfigMap = "test-config"
+	"sigs.k8s.io/yaml"
 )
 
 var _ = Describe("ConfigMap Controller", func() {
-	var testSrcNS corev1.Namespace
-	var testSrcCM corev1.ConfigMap
-	BeforeEach(func() {
-		By("Create test namespace")
-		testSrcNS = corev1.Namespace{ObjectMeta: v1.ObjectMeta{Name: testSrcNamespace}}
-		ctx := context.Background()
-		err := k8sClient.Create(ctx, &testSrcNS)
-		Expect(err).NotTo(HaveOccurred())
-		GinkgoLogr.Info("created test source namespace")
-		testSrcCM = corev1.ConfigMap{
-			ObjectMeta: v1.ObjectMeta{
-				Name:        testSrcConfigMap,
-				Namespace:   testSrcNamespace,
-				Annotations: map[string]string{syncKey: "app=myTestApp"},
-			},
-			Data: map[string]string{"HOST": "https://test-fake-kubed.io/"},
-		}
-
-		Eventually(k8sClient.Create(ctx, &testSrcCM)).WithTimeout(time.Second * 10).Should(Succeed())
-	})
-	// Context("When reconciling a resource", func() {
-
-	// 	It("should successfully reconcile the resource", func() {
-
-	// 		// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-	// 		// Example: If you expect a certain status condition after reconciliation, verify it here.
-	// 	})
-	// })
 	Context("When Namespace contains sync label", func() {
+		ctx := context.Background()
+		const (
+			testSrcNamespace = "test-src"
+			testSrcConfigMap = "test-config"
+			testLabelKey     = "app"
+			testLabelValue   = "myTestApp"
+			timeout          = time.Second * 10
+			interval         = time.Millisecond * 250
+		)
+		var testSrcNS corev1.Namespace
+		var testSrcCM corev1.ConfigMap
+		BeforeEach(func() {
+			By("Creating test source namespace")
+			testSrcNS = corev1.Namespace{ObjectMeta: v1.ObjectMeta{Name: testSrcNamespace}}
+			err := k8sClient.Create(ctx, &testSrcNS)
+			Expect(err).ToNot(HaveOccurred())
+			By("Creating test source configMap")
+			testSrcCM = corev1.ConfigMap{
+				ObjectMeta: v1.ObjectMeta{
+					Name:        testSrcConfigMap,
+					Namespace:   testSrcNamespace,
+					Annotations: map[string]string{syncKey: fmt.Sprintf("%s=%s", testLabelKey, testLabelValue)},
+				},
+				Data: map[string]string{"HOST": "https://test-fake-kubed.io/"},
+			}
+			err = k8sClient.Create(ctx, &testSrcCM)
+			Expect(err).ToNot(HaveOccurred())
+		})
 		It("should sync configMap to namespace", func() {
-			ctx := context.Background()
+			By("Creating target namespace that with the sync labels")
 			targetNamespace := "test-target"
-			err := k8sClient.Create(ctx, &corev1.Namespace{ObjectMeta: v1.ObjectMeta{Name: targetNamespace}})
-			Expect(err).NotTo(HaveOccurred())
+			ns := &corev1.Namespace{
+				ObjectMeta: v1.ObjectMeta{
+					Name:   targetNamespace,
+					Labels: map[string]string{testLabelKey: testLabelValue},
+				}}
+			ctx := context.Background()
 			cpConfigMap := &corev1.ConfigMap{}
-			err = k8sClient.Get(ctx, types.NamespacedName{Namespace: targetNamespace, Name: testSrcConfigMap}, cpConfigMap)
-			Expect(err).NotTo(HaveOccurred())
-			GinkgoLogr.Info(fmt.Sprintf("synced configMap: %v", cpConfigMap))
+			Expect(k8sClient.Create(ctx, ns)).NotTo(HaveOccurred())
+			b, _ := yaml.Marshal(ns)
+			log.Println(string(b))
+
+			By("Checking to see if the configmap was synced to target namespace")
+			lookupKey := types.NamespacedName{Name: testSrcConfigMap, Namespace: targetNamespace}
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, lookupKey, cpConfigMap)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+			b, _ = yaml.Marshal(cpConfigMap)
+			log.Println(string(b))
+
+			By("Checking configmap for source label name")
+			v, ok := cpConfigMap.Labels[sourceLabelName]
+			Expect(ok).Should(BeTrue())
+			Expect(v).Should(Equal(testSrcConfigMap))
+
+			By("Checking configmap for source label namespace")
+			v, ok = cpConfigMap.Labels[sourceLabelNamespace]
+			Expect(ok).Should(BeTrue())
+			Expect(v).Should(Equal(testSrcNamespace))
+
+			By("Checking configmap for finalizer")
+			Expect(cpConfigMap.Finalizers).Should(ContainElement(syncFinalizer))
 		})
 	})
 
